b33f063a81d767286d71c3c091f1742b05a42eb4
Eric Wallisch <ericwallisch@gmail.com>
2022-07-27 12:08:40 -0500

    Create Gameboard class

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class ship {
+class Ship {
     shipTypes = {
         'Carrier': 5,
         'Battleship': 4,
@@ -30,11 +30,50 @@ class ship {
 
 }
 
-const battleship = new ship('Battleship');
-console.log(battleship);
-battleship.hitShip(0);
-battleship.hitShip(1);
-battleship.hitShip(2);
-battleship.hitShip(3); 
-console.log(battleship);
-console.log(battleship.isSunk());
+class Gameboard {
+    constructor() {
+        this.board = [
+            new Array(10),
+            new Array(10),
+            new Array(10),
+            new Array(10),
+            new Array(10),
+            new Array(10),
+            new Array(10),
+            new Array(10),
+            new Array(10),
+            new Array(10),
+        ]
+    }
+
+    changeBoard(index, marker) {
+        let row = index[0];
+        let col = index[1];
+        this.board[row][col] = marker
+    }
+
+    placeShip(shipType, startPosition, direction) {
+        //take ship.length and array of position coordinates and add to board
+        const ship = new Ship(shipType)
+        let x = startPosition[0];
+        let y = startPosition[1];
+        if (direction === 'vertical') {
+            for (let i = 0; i < ship.length; i++) {
+                this.changeBoard([x,y], 'S');
+                x++;
+            }
+        }
+        else if (direction === 'horizontal') {
+            for (let i = 0; i < ship.length; i++) {
+                this.changeBoard([x,y], 'S');
+                y++;
+            }
+        }
+
+    }
+}
+
+const gameboard = new Gameboard();
+gameboard.placeShip('Carrier', [0,0], 'horizontal')
+gameboard.placeShip('Battleship', [5,5], 'vertical')
+console.log(gameboard.board)
---
+++ script.test.js
@@ -0,0 +1,40 @@
+import { Ship , Gameboard } from './script.js'
+
+test('create ship', () => {
+    const battleship = new Ship('Battleship');
+    expect(battleship).toBe(battleship);
+})
+
+test('hit ship', () => {
+    const battleship = new Ship('Battleship');
+    battleship.hitShip(0);
+    battleship.hitShip(1);
+    expect(battleship.hit).toEqual(['X','X',,])
+})
+
+test('see if ship is sunk - it is not', () => {
+    const battleship = new Ship('Battleship');
+    battleship.hitShip(0);
+    battleship.hitShip(1);
+    expect(battleship.isSunk()).toBe(false);
+})
+
+test('see if ship is sunk - it is', () => {
+    const battleship = new Ship('Battleship');
+    battleship.hitShip(0);
+    battleship.hitShip(1);
+    battleship.hitShip(2);
+    battleship.hitShip(3);
+    expect(battleship.isSunk()).toBe(true);
+})
+
+test('gameboard init creates gameboard array', () => {
+    const gameboard = new Gameboard();
+    expect(gameboard.board).toEqual(new Array(10).fill(new Array(10).fill(0)))
+})
+
+test('place ship in gameboard', () => {
+    const gameboard = new Gameboard();
+    gameboard.placeShip('Battleship', [0,0], 'vertical')
+    expect(gameboard.board).toEqual(new Array(10).fill(new Array(10).fill(0)))
+})
